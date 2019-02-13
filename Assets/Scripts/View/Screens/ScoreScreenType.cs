b797f1dbb187ab85dda47442eba080896b1eb827
Keith Chen <keith.chenzhun@yahoo.com>
2019-02-12 21:49:51 -0500

    Initial commit
    - added colored background to show playfield
    - added win condition max row
    - restyled score display text

---
+++ Assets/Scripts/View/Screens/ScoreScreenType.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class ScoreScreenType<T> : BaseScreen<T> where T : Component
+{
+    [SerializeField]
+    protected Text scoreText;
+
+    protected string mScorePrefix = "SCORE: ";
+
+    protected void SetScoreText(int value)
+    {
+        scoreText.text = mScorePrefix + value;
+    }
+}
